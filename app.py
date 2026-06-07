0a45d6714f923ab49a010bf12e21dcbce5f648eb
VISHWAS DESAI <dvishwas108@gmail.com>
2026-06-07 16:50:48 +0530

    Implement NLTK data download with caching
    Added a function to download NLTK data resources using caching.

--- app.py
+++ app.py
@@ -5,6 +5,14 @@ from nltk.corpus import stopwords
 import nltk
 from nltk.stem.porter import PorterStemmer
 
+@st.cache_resource
+def download_nltk_data():
+    nltk.download('punkt')
+    nltk.download('punkt_tab')
+    nltk.download('stopwords')
+
+download_nltk_data()
+
 ps = PorterStemmer()
 
 def transform_text(text):
